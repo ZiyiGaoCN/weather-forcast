63937841ead666b2ff251fa7032755c0056e4826
gaoziyi <gao-zy21@mails.tsinghua.edu.cn>
2024-01-03 10:11:32 +0000

    evaluation

---
+++ example/visualize.py
@@ -0,0 +1,76 @@
+import pandas
+import fire
+import numpy as np
+import matplotlib.pyplot as plt
+from loguru import logger
+
+def draws(name="",path=None,variables = None):
+    indices = [10,-1,-2,-3,45]
+
+    fig, axes = plt.subplots(2, len(indices), figsize=(20, 10)) 
+
+    rmses = np.load(f'evaluation/{path}/{name}rmses.npy')
+
+    np.set_printoptions(suppress=True)
+    
+    accs = np.load(f'evaluation/{path}/{name}accs.npy')
+
+    x = np.arange(0.25, rmses.shape[0]*0.25+0.25, 0.25)
+
+    for i, idx in enumerate(indices):
+        # RMSE plot
+        axes[0, i].plot(x,rmses[:,idx], label='RMSE')
+        axes[0, i].set_title(f'RMSE for {variables[i]}')
+        axes[0, i].set_xlabel('Measurement Index')
+        axes[0, i].set_ylabel('RMSE')
+        axes[0, i].legend()
+
+        # Accuracy plot
+        axes[1, i].plot(x,accs[:,idx], label='Accuracy')
+        axes[1, i].set_title(f'Accuracy for {variables[i]}')
+        axes[1, i].set_xlabel('Measurement Index')
+        axes[1, i].set_ylabel('Accuracy')
+        axes[1, i].legend()
+
+    plt.tight_layout()
+    plt.savefig(f'evaluation/{path}/{name}artifacts.png')
+    plt.show()
+
+    dict = {}
+    dict['model'] = path
+
+    for i, idx in enumerate(indices):
+        temp = [rmses[0,idx],rmses[3,idx],rmses[11,idx],rmses[19,idx]]
+        # 3 digit precision
+        temp = [round(x, 3) for x in temp]
+        output = '/'.join([str(x) for x in temp])
+        
+        dict[variables[i]] = output
+    
+    dict_acc ={}
+    
+    for i, idx in enumerate(indices):
+        temp = [accs[0,idx],accs[3,idx],accs[11,idx],accs[19,idx]]
+        # 3 digit precision
+        temp = [round(x, 3) for x in temp]
+        output = '/'.join([str(x) for x in temp])
+        
+        dict_acc[variables[i]] = output
+    
+    
+
+    logger.info(f'RMSE for {name} is {dict}')
+    logger.info(f'Accuracy for {name} is {dict_acc}')
+    df = pandas.DataFrame(dict, index=[0], columns=['model','t850', 't2m', '10v', '10u', 'z500'])
+    df.to_csv(f'evaluation/{path}/{name}rmse.csv', index=False)
+
+def artifacts (
+    path='multivarible-swin-6token-normal-finetune',
+    variables = [ 't850', 't2m', '10v', '10u', 'z500'] ):
+    draws(name="train_",path=path,variables=variables)
+    draws(name="",path=path,variables=variables)
+    
+import fire
+
+if __name__ == '__main__':
+    fire.Fire(artifacts)
